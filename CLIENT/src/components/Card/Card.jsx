afdfe5a007854289e61d4326ec9e64ed54041b7d
JuanM <juanmanesevich25@gmail.com>
2022-10-13 15:28:12 -0300

    agregue rutas en App para los componentes que faltaban, agregué NavBar al Home y corregí algunos errores de sintaxis

--- CLIENT/src/components/Card/Card.jsx
+++ CLIENT/src/components/Card/Card.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Card = ({ img, title, price, id }) => {
   return (
     <div className="container">
-      <Link to={`/Home/Product/${id}`}>
+      <Link to={`/home/product/${id}`}>
         <img
           className="w-25 p-3"
           src="https://arturocalle.vtexassets.com/arquivos/ids/473103/HOMBRE-CAMISA-10122546-AZUL-780_1.jpg?v=637950736212900000"
           alt="shirt"
         />
       </Link>
-      <h3>Tittle</h3>
+      <h3>Title</h3>
       <h3>Price</h3>
     </div>
   );
