e44778581337207b90e01500fdeab0709c4f53d6
Martin Mendoza <martinmendoza-2000@hotmail.com>
2022-10-16 17:08:21 -0300

    Agregando estilos
    Landing page completamente estilizado y cartas de los productos estilizados

--- CLIENT/src/components/Card/Card.jsx
+++ CLIENT/src/components/Card/Card.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Card.css"
 
 const Card = ({ img, title, price, id }) => {
   return (
-    <div className="container">
-      <Link to={`/Home/Product/${id}`}>
-        <img
-          className="w-25 p-3"
-          src="https://arturocalle.vtexassets.com/arquivos/ids/473103/HOMBRE-CAMISA-10122546-AZUL-780_1.jpg?v=637950736212900000"
-          alt="shirt"
-        />
+    <div className="Card">
+      <Link className="Link" to={`/Home/Product/${id}`}>
+        <div className="ImgContainer">
+          <img
+            className="ProductIMG"
+            /*className="w-25 p-3"*/
+            src="https://arturocalle.vtexassets.com/arquivos/ids/473103/HOMBRE-CAMISA-10122546-AZUL-780_1.jpg?v=637950736212900000"
+            alt="shirt"
+          />
+        </div>
+        <h3>Tittle</h3>
+        <h3>Price</h3>
       </Link>
-      <h3>Tittle</h3>
-      <h3>Price</h3>
     </div>
   );
 };
