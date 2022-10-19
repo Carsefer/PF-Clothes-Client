e04a83682266335c24e1dd5bde2b0a43a536becd
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-10-19 09:30:56 -0500

    bug crear usuario

--- CLIENT/src/redux/reducer/index.js
+++ CLIENT/src/redux/reducer/index.js
@@ -15,7 +15,6 @@ import {
   REMOVE_ALL_FROM_CART,
   REMOVE_ONE_FROM_CART,
   GET_REVIEWS_PRODUCT_DETAIL,
-
 } from "../action-types";
 
 const initialState = {
@@ -26,7 +25,7 @@ const initialState = {
   sizes: [],
   marks: [],
   productsStatus: "loading",
-  cart: []
+  cart: [],
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -42,7 +41,7 @@ const rootReducer = (state = initialState, action) => {
         ...state,
         productDetail: action.payload,
       };
-    case GET_REVIEWS_PRODUCT_DETAIL: 
+    case GET_REVIEWS_PRODUCT_DETAIL:
       return {
         ...state,
         productReviews: action.payload,
@@ -109,57 +108,55 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
       };
-      case ADD_TO_CART: {
-        let newItem = state.products.find(
-          (product) => product.id === action.payload
-        );
-        
-  
-        let itemInCart = state.cart.find((item) => item.id === newItem.id);
-  
-        return itemInCart
-          ? {
-              ...state,
-              cart: state.cart.map((item) =>
-                item.id === newItem.id
-                  ? { ...item, quantity: item.quantity + 1 }
-                  : item
-              ),
-            }
-          : {
-              ...state,
-              cart: [...state.cart, { ...newItem, quantity: 1 }],
-            };
-      }
-      case REMOVE_ONE_FROM_CART: {
-        let itemToDelete = state.cart.find((item) => item.id === action.payload);
-  
-        return itemToDelete.quantity > 1
-          ? {
-              ...state,
-              cart: state.cart.map((item) =>
-                item.id === action.payload
-                  ? { ...item, quantity: item.quantity - 1 }
-                  : item
-              ),
-            }
-          : {
-              ...state,
-              cart: state.cart.filter((item) => item.id !== action.payload),
-            };
-      }
-      case REMOVE_ALL_FROM_CART: {
-        return {
-          ...state,
-          cart: state.cart.filter((item) => item.id !== action.payload),
-        };
-      }
-      case CLEAR_CART: {
-        return {
-          ...state,
-        }
+    case ADD_TO_CART: {
+      let newItem = state.products.find(
+        (product) => product.id === action.payload
+      );
+
+      let itemInCart = state.cart.find((item) => item.id === newItem.id);
+
+      return itemInCart
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === newItem.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            ),
+          }
+        : {
+            ...state,
+            cart: [...state.cart, { ...newItem, quantity: 1 }],
+          };
+    }
+    case REMOVE_ONE_FROM_CART: {
+      let itemToDelete = state.cart.find((item) => item.id === action.payload);
+
+      return itemToDelete.quantity > 1
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === action.payload
+                ? { ...item, quantity: item.quantity - 1 }
+                : item
+            ),
+          }
+        : {
+            ...state,
+            cart: state.cart.filter((item) => item.id !== action.payload),
+          };
+    }
+    case REMOVE_ALL_FROM_CART: {
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
       };
-        
+    }
+    case CLEAR_CART: {
+      return {
+        ...state,
+      };
+    }
 
     default:
       return state;
