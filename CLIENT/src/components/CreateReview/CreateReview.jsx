7c1ec30047fc95bb169f881195dd7452d23e2f27
Martin Mendoza <martinmendoza-2000@hotmail.com>
2022-10-20 12:26:59 -0300

    Update CreateReview.jsx

--- CLIENT/src/components/CreateReview/CreateReview.jsx
+++ CLIENT/src/components/CreateReview/CreateReview.jsx
@@ -19,7 +19,7 @@ const CreateReview = ({id}) => {
         <div>
             <h2>Realizar Reseña</h2>
             <div>
-                <Rating precision={0.5} value={rating.value} onChange={(event, newvalue) => {setRating({...rating, value: newvalue});}} sx={{fontSize: '5rem',}}/>
+                <Rating value={rating.value} onChange={(event, newvalue) => {setRating({...rating, value: newvalue});}} sx={{fontSize: '5rem',}}/>
             </div>
             <div>
                 <form onSubmit={(e) => {
