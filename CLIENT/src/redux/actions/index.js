f039084927af20ee04816352490f92e821a1cbb0
Juan Pablo Romero <jryn145@gmail.com>
2022-10-31 00:30:52 -0400

    update

--- CLIENT/src/redux/actions/index.js
+++ CLIENT/src/redux/actions/index.js
@@ -286,7 +286,7 @@ export const getSellDetail = (idSell) => {
 export const createReviewProduct = (id, data, token) => {
   return async (dispatch) => {
     const res = await axios.post(
-      `/product/review/${id}&secret_token=${token}`,
+      `/product/review/${id}?secret_token=${token}`,
       data
     );
     dispatch({
