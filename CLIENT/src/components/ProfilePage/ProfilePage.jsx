3c2af9184dbc57fa663c61e409f7fc36e455f8c5
Martin Mendoza <martinmendoza-2000@hotmail.com>
2022-11-02 16:53:11 -0300

    s
    s

--- CLIENT/src/components/ProfilePage/ProfilePage.jsx
+++ CLIENT/src/components/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Profile from "../Profile/Profile";
 import SellPage from "../SellPage/SellPage";
 import BuyPage from "../BuyPage/BuyPage";
@@ -7,15 +7,27 @@ import NavBar from "../NavBar/NavBar";
 import { Link } from "react-router-dom";
 import Styles from "./ProfilePage.module.css"
 
-const ProfilePage = () => {
+const ProfilePage = () => {  
+    const location = useLocation();
+
+    console.log(location.pathname)
+
     return (
         <div className={Styles.ProfilePage}>
             <NavBar />
             <div className={Styles.profileLinksDiv}>
-                <Link className={Styles.profileLinks} to="/home/profile">PERFIL</Link>
-                <Link className={Styles.profileLinks} to="/home/profile/buys">COMPRAS</Link>
-                <Link className={Styles.profileLinks} to="/home/profile/sells">VENTAS</Link>
-                <Link className={Styles.profileLinks} to="/home/stadistics">ESTADISTICAS</Link>
+                <Link id={location.pathname === "/home/profile" ? Styles.Selected : Styles.NotSelected} 
+                    className={Styles.profileLinks} to="/home/profile">PERFIL
+                </Link>
+                <Link id={location.pathname === "/home/profile/buys" ? Styles.Selected : Styles.NotSelected} 
+                    className={Styles.profileLinks} to="/home/profile/buys">COMPRAS
+                </Link>
+                <Link id={location.pathname === "/home/profile/sells" ? Styles.Selected : Styles.NotSelected} 
+                    className={Styles.profileLinks} to="/home/profile/sells">VENTAS
+                </Link>
+                <Link id={location.pathname === "/home/profile/stadistics" ? Styles.Selected : Styles.NotSelected} 
+                    className={Styles.profileLinks} to="/home/stadistics">ESTADISTICAS
+                </Link>
             </div>
             <div className={Styles.profileRoutes}>
                 <Routes>
